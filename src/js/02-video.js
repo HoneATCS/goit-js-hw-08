b0a3de6c8e892ebe860d4599cd7b6348bd442be3
ESM <111918644+HoneATCS@users.noreply.github.com>
2023-02-06 23:51:45 +0200

    correct

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_KEY = "videoplayer-current-time";
 
+player.on('timeupdate', onPlay);
+function onPlay({seconds}) {
+    console.log(seconds);
+    localStorage.setItem(STORAGE_KEY, seconds);
+}
+    
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY) || 0);
+
+
+player.on('play', onPlay);
+
+player.on('timeupdate', trottle(onPlay, 1000));
